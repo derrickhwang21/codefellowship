7df3cbfcbc3b4200fdf165081610f9ae3f060674
dhwang21 <hwang.derrick@gmail.com>
2018-12-25 16:21:57 -0800

    working login

--- src/main/java/com/hwang/secure/SecureApplication.java
+++ src/main/java/com/hwang/secure/SecureApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
     public class SecureApplication {
 
         public static void main(String[] args) {
-            SpringApplication.run(SecureApplication.class, args );
+            SpringApplication.run(SecureApplication.class, args);
             System.out.println("http://localhost:8080");
 
 
---
+++ src/main/java/com/hwang/secure/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.hwang.secure;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsByNameServiceWrapper;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService  {
+
+    @Autowired
+    private ApplicationUserRepository applicationUserRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        ApplicationUser applicationUser = this.applicationUserRepo.findByUsername(username);
+
+        if (applicationUser == null) {
+            throw new UsernameNotFoundException(username + " was not found.");
+        }
+
+        return applicationUser;
+    }
+
+
+}
